325f597f9b10b3c46a75f52a43f92e9489b5caad
o.Outlandish1 <tpl1219_13@TPL1219-11.attlocal.net>
2019-12-14 13:19:47 -0800

    First Commit

---
+++ Pre-Work-Week1.js
@@ -0,0 +1,83 @@
+var emptyVariable = '';
+    myName = 'Robyn';
+    dreamDestination = 'Berlin';
+    num1 = 3; //value greater than 1 and less than 10
+    num2 = 9; //value of greater than or equal to 1 and less than or equal to 10
+
+//Question:
+//What numbers would be valid values for num2 but not num1?
+//Answer:
+//num1 is exclusive of 1 and 10, while num2 is inclusive of 1 and 10. So, 1 and 10 would be valid values for num2 but not num1.
+
+//
+//
+//
+
+var num1 = 6;
+    num2 = 4;
+    theSum = num1 + num2;
+    theProduct = num1 * num2;
+    
+//
+//
+//
+
+var myName = 'Robyn';
+    dreamDestination = 'Berlin';
+	part1 = 'Hi my name is' + ' ' + myName + ' ';
+    part2 = 'and I can\'t wait to visit' + ' ' + dreamDestination + '!';
+    statement = part1.concat(part2);
+
+//
+//
+//
+
+// Exercise 10. What is the data type of the value stored in 'dreamDestination',
+// place your answer in the quotes below:
+var myAnswerForExercise10 = 'string';
+
+// Exercise 11. What is the data type of the value stored in 'product',
+// place your answer in the quotes below:
+var myAnswerForExercise11 = 'number'
+
+// Exercise 12. What do you think the value of emptyVariable is?
+// Place your answer below:
+var myAnswerForExercise12 = 'whitespace';
+
+// Exercise 13. If
+var A = "R";
+var B = 1;
+var C = 4;
+var D = "D";
+
+// What is the value of B + C
+var myAnswerForExercise13 = '5';
+
+// Exercise 14 What is the value of
+// A + "n" + D
+var myAnswerForExercise14 = 'RnD';
+
+// Exercise 15 What is the value of
+// A + B * 2 + D + C / 2:
+var myAnswerForExercise15 = 'R2D2';
+
+// Exercise 16 What is the value of A * B
+var myAnswerForExercise16 = 'NaN';
+
+// Exercise 17 What is the value of true || false
+var myAnswerForExercise17 = 'true';
+
+// Exercise 18 What is the value of 1 === "1"
+var myAnswerForExercise18 = 'false';
+
+// Exercise 19 What is the value of 1 == "1"
+var myAnswerForExercise19 = 'true';
+
+// Exercise 20 What is the value of 1 < 1
+var myAnswerForExercise20 = 'false';
+
+// Exercise 21 What is the value of 1 < 2 < 3
+var myAnswerForExercise21 = 'true';
+
+// Exercise 22 What is the value of 2 < 1 < 3
+var myAnswerForExercise22 = 'true';
